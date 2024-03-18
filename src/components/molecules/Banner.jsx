27b05174289d6722ea25798b0816481439b15d7a
Claudia González <claudia.glezg@gmail.com>
2024-03-18 11:30:46 +0100

    add Banner component

---
+++ src/components/molecules/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="w-full flex justify-center bg-beige-bg">
+    <div className="max-w-screen-xl mx-auto p-0 mt-10">
+      <img src="src\assets\images\BannerGrandeBeige.svg" className="w-full" alt="Mon Petit Parapluie Banner" />
+    </div>
+  </div>
+  )
+}
+
+export default Banner
---
+++ src/components/views/HomeView.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../molecules/Navbar'
+import Banner from '../molecules/Banner'
+
+const HomeView = () => {
+  return (
+    <>
+   <Navbar />
+   <Banner />
+   </>
+  )
+}
+
+export default HomeView
