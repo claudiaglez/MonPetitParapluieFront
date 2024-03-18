6c12816c4424b49f54e1d9b82553f8f593fdb4ef
Claudia González <claudia.glezg@gmail.com>
2024-03-18 12:34:49 +0100

    add modify gallery

--- src/components/molecules/Gallery.jsx
+++ src/components/molecules/Gallery.jsx
@@ -1,43 +1,54 @@
-import React from 'react';
+import React from 'react'
 
 const Gallery = () => {
-  return (
-    <div className="grid grid-cols-3 grid-rows-3 gap-4 bg-beige-bg mt-gallery">
-      <div className="grid gap-4">
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/minnie.jpg" alt="" />
+    return (
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 bg-beige-bg">
+        <div className="grid gap-4">
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/marie.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/cafeparis.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/marypoppins.jpg" alt="" />
+          </div>
         </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/cafeparis.jpg" alt="" />
+        <div className="grid gap-4">
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/gatofolklore.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/gnomos.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/mafalda.jpg" alt="" />
+          </div>
         </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/bellaleyendo.jpg" alt="" />
+        <div className="grid gap-4">
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/principito.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/nicolas.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/perritowc.jpg" alt="" />
+          </div>
+        </div>
+        <div className="grid gap-4">
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/queen.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/ironmaiden.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/minnie.jpg" alt="" />
+          </div>
         </div>
       </div>
-      <div className="grid gap-4">
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/gatofolklore.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/principito.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/mafalda.jpg" alt="" />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/gnomos.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/ironmaiden.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/perritowc.jpg" alt="" />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Gallery;
+    );
+  };
+  
+  export default Gallery;
--- src/components/views/HomeView.jsx
+++ src/components/views/HomeView.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../views/homeView.css'
 import Navbar from '../molecules/Navbar'
 import Banner from '../molecules/Banner'
 import Gallery from '../molecules/Gallery'
@@ -6,9 +7,11 @@ import Gallery from '../molecules/Gallery'
 const HomeView = () => {
   return (
     <>
+    <div className='homeViewContainer'>
    <Navbar />
    <Banner />
    <Gallery />
+   </div>
    </>
   )
 }
