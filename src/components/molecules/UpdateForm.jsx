96e28daef2766ed88e406d4e1748120b9e3e89bc
Claudia González <claudia.glezg@gmail.com>
2024-03-19 11:47:30 +0100

    add delete function in UpdateForm

--- src/components/molecules/UpdateForm.jsx
+++ src/components/molecules/UpdateForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const UpdateForm = ({ article }) => {
+const navigate = useNavigate();
   const [categories, setCategories] = useState([]);
   const [formData, setFormData] = useState({
     title: '',
@@ -70,6 +72,26 @@ const UpdateForm = ({ article }) => {
     fetchCategories();
   }, []);
 
+    // Función para borrar el artículo
+    const handleDelete = async () => {
+        try {
+          const response = await fetch(`http://127.0.0.1:8000/api/articles/${article.id}`, {
+            method: 'DELETE'
+          });
+          if (!response.ok) {
+            // Si la respuesta no es exitosa, lanzar un error con el mensaje del servidor
+            const errorMessage = await response.text();
+            throw new Error(errorMessage || 'Error al borrar el producto');
+          }
+          alert('Producto borrado correctamente');
+          // Redirigir al usuario a la página anterior después de borrar el artículo
+         navigate('/articles');
+        } catch (error) {
+          console.error(error);
+          alert('Error al borrar el producto');
+        }
+      };
+
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="max-w-2xl px-4 py-8 mx-auto lg:py-16">
@@ -102,7 +124,7 @@ const UpdateForm = ({ article }) => {
             <button type="submit" className="text-white bg-pomme hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
               Actualizar Producto
             </button>
-            <button type="button" className="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+            <button type="button" onClick={handleDelete} className="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
               <svg className="w-5 h-5 mr-1 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd"></path></svg>
               Borrar
             </button>
