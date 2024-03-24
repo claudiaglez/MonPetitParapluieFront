50b6460a8cfe396e6085d5abcdc1386f6e6e856a
Claudia González <claudia.glezg@gmail.com>
2024-03-24 21:26:46 +0100

    add QueTeBordoView

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -7,6 +7,7 @@ import ArticleDetailsView from "../components/views/ArticleDetailsView";
 import CategoryView from "../components/views/CategoryView";
 import UpdateArticleView from "../components/views/UpdateArticleView";
 import ProyectoView from "../components/views/ProyectoView";
+import QueTeBordoView from "../components/views/QueTeBordoView";
 
 const App = () => {
   const [articles, setArticles] = useState([]);
@@ -33,6 +34,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomeView/>}></Route>
         <Route path='/proyecto' element={<ProyectoView/>}></Route>
+        <Route path='/quetebordo' element={<QueTeBordoView/>}></Route>
         <Route path='/form' element={<FormView/>}></Route>
         {/* Pasamos los artículos como prop a ArticlesView y ArticleDetailsView */}
         <Route path='/articles' element={<ArticlesView articles={articles} />}></Route>
